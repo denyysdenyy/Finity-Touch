012a7ccd2cdeb19ef4a321efdc126c55a2e25ed3
denyys <denyysitbelikov@gmail.com>
2023-04-05 00:37:47 +0300

    fund questions done

--- src/js/modules/accordion.js
+++ src/js/modules/accordion.js
@@ -1,12 +1,28 @@
 function handleQuestionsAccordionClass(){
     const accordion = document.getElementsByClassName('questions__accordion');
-    const accordionBody = document.getElementsByClassName('questions__accordion-body');
-    console.log(accordion)
-    for(let i = 0; i < accordion.length; i++){
-       accordion[i].addEventListener('click', function(){
-        this.classList.toggle('active')
-       });
+    if(accordion){
+        for(let i = 0; i < accordion.length; i++){
+            accordion[i].addEventListener('click', function(){
+             this.classList.toggle('active')
+             console.log('open')
+            });
+         }
     }
+   
 }
 handleQuestionsAccordionClass()
+
+function handlerFundQuestionsAccordionClass(){
+    const accordion = document.getElementsByClassName('fund__accordion');
+    if(accordion){
+        for(let i = 0; i < accordion.length; i++){
+            accordion[i].addEventListener('click', function(){
+             this.classList.toggle('active')
+            });
+         }
+    }
+   
+}
+handlerFundQuestionsAccordionClass()
+
 
--- src/js/modules/showcards.js
+++ src/js/modules/showcards.js
@@ -1,9 +1,13 @@
 const teamButton = document.getElementById('team__button');
+if(teamButton){
+    teamButton.addEventListener('click',showCards);
+}
 function showCards(){
     const teamCard = document.getElementsByClassName('team__card');
+    if(teamCard){
         for(let i = 8; i < teamCard.length; i++){
             teamCard[i].classList.toggle('hidden')
         }
+    }
 }
 
-teamButton.addEventListener('click',showCards);
